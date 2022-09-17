77d390c52aadbca191964d7d4831b2adc3da2234
Alfred Turkson <111619866+Codeghod@users.noreply.github.com>
2022-09-17 16:56:27 -0700

    Add files via upload

---
+++ conditional statements.py
@@ -0,0 +1,26 @@
+# Multiple excsercises on conditional statements
+
+age = int(input('enter your age: '))
+if age<18:
+    print('you are not qualified to vote')
+else:
+    print('you are elegible to vote')
+    
+number = int(input('Enter digit: '))
+if number%2==0:
+    print('the number entered is an even number')
+else:
+    print('the number entered is odd')
+    
+numer = int(input('enter number: '))
+if numer%5 == 0:
+    print('hello')
+else:
+    print('bye')
+    
+number = (input("Enter number: "))
+l = len(number)
+if l==3:
+    print("good")
+else:
+    print("bad")
